b267577b9ed50985195423dc820030ecedc5aa22
esgn <5435148+esgn@users.noreply.github.com>
2026-03-27 17:03:24 +0100

    feat(export): export collection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import type { Collection } from "./types";
 import { loadCollections } from "./services/storage";
+export type Collection = import("./types").Collection;
+export type CollectionProperty = import("./types").CollectionProperty;
 
 export function getCollections(): Collection[] {
     return structuredClone(loadCollections());
