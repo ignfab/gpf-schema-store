8622fc2386344737b12e80fc60354abfffc344f7
MBorne <mickael.borne@ign.fr>
2026-03-27 15:47:35 +0100

    chore(build): fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import type { Collection } from "./types";
+import { loadCollections } from "./services/storage";
 
-const collections = getCollections();
+const collections = loadCollections();
 
-export function getCollections(): Promise<Collection[]> {
+export function getCollections(): Collection[] {
     return collections;
 }
