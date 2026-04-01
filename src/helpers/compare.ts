c25882075c3d2cd432c217364fcb3b96d37cbf2e
MBorne <mickael.borne@ign.fr>
2026-04-01 10:23:06 +0200

    chore(compare): create a dedicated function to simplify logging and testing (refs #8)

---
+++ src/helpers/compare.ts
@@ -0,0 +1,24 @@
+import type { Collection } from "..";
+
+/**
+ * Compare properties between original and overwrite to ensure that both are synchronized.
+ * 
+ * @param original The collection from data/wfs
+ * @param overwrite The collection from data/overwrite
+ * @returns 
+ */
+export function compare(original: Collection, overwrite: Collection): string[] {
+    const originalNames = new Set(original.properties.map((p) => p.name))
+    const overwriteNames = new Set(overwrite.properties.map((p) => p.name))
+    const onlyInOriginal = [...originalNames].filter((n) => !overwriteNames.has(n))
+    const onlyInOverwrite = [...overwriteNames].filter((n) => !originalNames.has(n))
+
+    const differences = [
+        ...onlyInOriginal.map((propertyName) => `Property "${propertyName}" missing in overwrite`),
+        ...onlyInOverwrite.map((propertyName) => `Property "${propertyName}" is not present in WFS collection`),
+    ];
+
+    return differences;
+}
+
+
